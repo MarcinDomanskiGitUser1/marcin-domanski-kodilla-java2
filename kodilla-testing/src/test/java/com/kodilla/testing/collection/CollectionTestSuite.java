c6e999ea4a13984f1d9fc39cb752a2e0a0f14632
MarcinDomanskiGitUser1 <marcindomanski@Mac-mini-Marcin.local>
2018-05-31 14:43:17 +0200

    Dodane testy do klasy filtrującej liczby parzyste

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,21 +1,61 @@
 package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumberExterminator;
 
-
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 
 public class CollectionTestSuite {
-    @Test
+
+
+    @Before
+    public void beforeTestEmptyList() {
+        System.out.println("Start test");
+    }
+
+@Test
     public void testOddNumbersExterminatorEmptyList() {
 
         ArrayList<Integer> emptyList = new ArrayList<Integer>();
 
-        System.out.println(emptyList.size());
-
         OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
 
-        emptyListSize = oddNumberExterminator.exterminate(emptyList);
+        System.out.println("Empty list size is -");
+        oddNumberExterminator.exterminate(emptyList);
+
+        Assert.assertEquals(emptyList.size(), oddNumberExterminator.evenNumbers.size());
+
+        System.out.println("Even numbers list size is " + oddNumberExterminator.evenNumbers.size());
+    }
+
+    @After
+    public void afterTestEmptyList () {
+        System.out.println("End test");
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList () {
+        ArrayList<Integer> normalList = new ArrayList<Integer>();
+        normalList.add(4);
+        normalList.add(5);
+        normalList.add(7);
+        normalList.add(12);
+        normalList.add(15);
+        normalList.add(24);
+
+
+    OddNumberExterminator oddNumberExterminator = new OddNumberExterminator();
+
+        oddNumberExterminator.exterminate(normalList);
+
+        System.out.println("Normal list first position" + normalList.get(0));
+
+        System.out.println("Even numbers list first position " + oddNumberExterminator.evenNumbers.get(0));
+
+        Assert.assertEquals(normalList.get(0), oddNumberExterminator.evenNumbers.get(0));
 
     }
+
 }
