bc907f81cf01998b0f3ed3e52e8494bb16d3c443
MarcinDomanskiGitUser1 <marcindomanski@Mac-mini-Marcin.local>
2018-05-31 19:52:49 +0200

    Klasa filtrująca liczby parzyste

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumberExterminator.java
@@ -4,41 +4,19 @@ import java.util.*;
 
 public class OddNumberExterminator {
 
-    private ArrayList<Integer> evenNumbers;
+    public List<Integer> exterminate(List<Integer> numbers) {
 
-    public OddNumberExterminator() {
-        evenNumbers = new ArrayList<Integer>();
-    }
+        List<Integer> evenNumbers = new ArrayList<Integer>();
 
-    public void exterminate (ArrayList<Integer> numbers) {
-
-        for (int n=0; n < numbers.size(); n++){
+        for (int n = 0; n < numbers.size(); n++) {
 
             int temporaryValue = numbers.get(n);
 
-                if (temporaryValue % 2 == 0){
+            if (temporaryValue % 2 == 0) {
 
                 evenNumbers.add(temporaryValue);
-                }
+            }
         }
-
-        System.out.println(evenNumbers.size());
-    }
-}
-
-class OddTaker
-{
-    public static void main (String[] args) {
-
-        ArrayList<Integer> redList = new ArrayList<Integer>();
-
-        for(int n=0; n < 100; n++) {
-            redList.add(n);
-        }
-
-        OddNumberExterminator oddExterminator = new OddNumberExterminator();
-        oddExterminator.exterminate(redList);
-
-
+        return evenNumbers;
     }
 }
