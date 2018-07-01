2141715e67d29ecd3213498d22b8bddf486b5d6d
marcin domański <>
2018-07-01 16:47:01 +0200

    Dodane wykorzystanie klas Book i BookDirectory. Użyte metody collect, toList, toMap, joining.

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,28 +1,33 @@
 package com.kodilla.stream;
 
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.person.People;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class StreamMain {
     public static void main(String[] args) {
         ExpressionExecutor expressionExecutor = new ExpressionExecutor();
 
-        System.out.println("--Calculating expressions with lambdas--");
+        System.out.println("--------------Calculating expressions with lambdas--");
         expressionExecutor.executeExpression(10, 5, ((a, b) -> a + b));
         expressionExecutor.executeExpression(10, 5, ((a, b) -> a - b));
         expressionExecutor.executeExpression(10, 5, ((a, b) -> a * b));
         expressionExecutor.executeExpression(10, 5, ((a, b) -> a / b));
 
-        System.out.println("--Calculating expressions with method references--");
+        System.out.println("--------------Calculating expressions with method references--");
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        System.out.println("--Using string methods and lambda--");
+        System.out.println("--------------Using string methods and lambda--");
         PoemBeautifier poemBeautifier = new PoemBeautifier();
         String someText = "This Text Is Decorate With Lambda";
         poemBeautifier.beautify(someText, text -> text.toLowerCase());
@@ -37,14 +42,14 @@ public class StreamMain {
             return result;
         });
 
-        System.out.println("--Using Stream to generate even numbers from 1 to 20--");
+        System.out.println("--------------Using Stream to generate even numbers from 1 to 20--");
         NumbersGenerator.generateEven(20);
 
-        System.out.println("--Using Stream to check my getList method--");
+        System.out.println("--------------Using Stream to check my getList method--");
         People.getList().stream()
                 .forEach(System.out::println);
 
-        System.out.println("--Using map method on my getList method--");
+        System.out.println("--------------Using map method on my getList method--");
         People.getList().stream()
                 .map(s -> s.toUpperCase())
                 .forEach(System.out::println);
@@ -53,17 +58,51 @@ public class StreamMain {
                 .map(String::toUpperCase) //Replace lambda to referance to method
                 .forEach(s -> System.out.println(s)); //Replace referance to method to lambda.
 
-        System.out.println("--Shows if text have more letters than 11--");
+        System.out.println("--------------Shows if text have more letters than 11--");
         People.getList().stream()
                 .filter(s -> s.length() > 11)
                 .forEach(System.out::println);
 
-        System.out.println("--Using cascade filters in stream--");
+        System.out.println("--------------Using cascade filters in stream--");
         People.getList().stream()
                 .map(String::toUpperCase)
                 .filter(s -> s.length() > 11)
                 .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
                 .filter(s -> s.substring(0, 1).equals("M"))
                 .forEach(System.out::println);
+
+        System.out.println("--------------Using multi classes with stream--");
+        BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getTheBookList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);
+
+        System.out.println("--------------Using collect() method and collectors.tolist()--");
+        //BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getTheBookList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toList());
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream()
+                .forEach(System.out::println);
+
+        System.out.println("-------------Using collectors.toMap()--");
+        //BookDirectory theBookDirectory = new BookDirectory();
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getTheBookList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+        System.out.println("--------------Using collectors.joining()--");
+        //BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getTheBookList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining("\nnext entry//\n","<<",">>"));
+        System.out.println(theResultStringOfBooks);
+
     }
 }
