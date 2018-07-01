e3b321569bc1ec9e9023d1109ad2b222fb922d52
marcin domański <>
2018-07-01 14:01:13 +0200

    Użyte metody stream, map, filter

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,8 +1,10 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.person.People;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -34,5 +36,34 @@ public class StreamMain {
                 result = result + (" " + tex.charAt(i));
             return result;
         });
+
+        System.out.println("--Using Stream to generate even numbers from 1 to 20--");
+        NumbersGenerator.generateEven(20);
+
+        System.out.println("--Using Stream to check my getList method--");
+        People.getList().stream()
+                .forEach(System.out::println);
+
+        System.out.println("--Using map method on my getList method--");
+        People.getList().stream()
+                .map(s -> s.toUpperCase())
+                .forEach(System.out::println);
+        //Replace
+        People.getList().stream()
+                .map(String::toUpperCase) //Replace lambda to referance to method
+                .forEach(s -> System.out.println(s)); //Replace referance to method to lambda.
+
+        System.out.println("--Shows if text have more letters than 11--");
+        People.getList().stream()
+                .filter(s -> s.length() > 11)
+                .forEach(System.out::println);
+
+        System.out.println("--Using cascade filters in stream--");
+        People.getList().stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.length() > 11)
+                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+                .filter(s -> s.substring(0, 1).equals("M"))
+                .forEach(System.out::println);
     }
 }
