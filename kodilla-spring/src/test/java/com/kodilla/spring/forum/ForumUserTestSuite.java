da31d2227ea4347eab16bb9b39eb73e0886f0097
marcin domański <>
2018-08-17 16:34:06 +0200

    Task 10.1 (@Component, Bean)

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.forum;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ForumUserTestSuite {
+
+    @Test
+    public void testGetUserName() {
+
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
+        ForumUser user = context.getBean(ForumUser.class);
+
+        //When
+        String userN = user.userName;
+
+        //Then
+        Assert.assertEquals("John Wielebny Smith", userN);
+    }
+}
