291ad3f123077716630dc7f3fc5e9492b79408d5
marcin domański <>
2018-08-18 13:05:28 +0200

    Task 10.2 (Springboot, Autowired, Component)

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -6,32 +6,26 @@ import org.springframework.stereotype.Component;
 @Component
 public class Calculator {
 
-    private double val;
-
     @Autowired
     private Display display;
 
     public double add(double a, double b) {
-        val = a + b;
-        display.displayValue(val);
-        return val;
+        display.displayValue(a + b);
+        return a + b;
     }
 
     public double sub(double a, double b) {
-        val = a - b;
-        display.displayValue(val);
-        return val;
+        display.displayValue(a - b);
+        return a - b;
     }
 
     public double mul(double a, double b) {
-        val = a * b;
-        display.displayValue(val);
-        return val;
+        display.displayValue(a * b);
+        return a * b;
     }
 
     public double div(double a, double b) {
-        val = a / b;
-        display.displayValue(val);
-        return val;
+        display.displayValue(a / b);
+        return a / b;
     }
 }
